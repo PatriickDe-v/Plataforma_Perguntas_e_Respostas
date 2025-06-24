8cbc6455c41dbe56fac28fa34800eb943a3d508f
Patrick <patriick.dev@hotmail.com>
2025-06-24 15:51:03 -0300

    reutilizando HTML com partials

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const app = express()              //criando uma copia do express
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+//rota padrao, home 
 app.get("/", (req, res) => {
     res.render("index")
 })
-
+//rota perguntar
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar")
+})
 
 app.listen(8080, () => {
     console.log("App rodando")
